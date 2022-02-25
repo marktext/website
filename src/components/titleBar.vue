52e0a7152653d32331f181301d3722dc45894c33
laggage <laggage-la@outlook.com>
2022-02-26 01:12:45 +0800

    fix: fix bad app download link for mac

--- src/components/titleBar.vue
+++ src/components/titleBar.vue
@@ -25,7 +25,7 @@
         <div class="download-group">
           <div class="download-info">Available for macOS, Windows and Linux.</div>
           <div class="button-group">
-            <a class="button" href="https://github.com/marktext/marktext/releases/latest/download/marktext.dmg" target="_blank">
+            <a class="button" href="https://github.com/marktext/marktext/releases/latest/download/marktext-x64.dmg" target="_blank">
               <svg :viewBox="MacIcon.viewBox" aria-hidden="true" class="icon">
                 <use :xlink:href="MacIcon.url" />
               </svg>
