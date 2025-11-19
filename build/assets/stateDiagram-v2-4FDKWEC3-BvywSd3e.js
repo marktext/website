cd456c6ee6c2d004c24810dfe93d19e9cb970c47
Jocs <luoran1988@126.com>
2025-11-19 10:27:06 +0800

    chore: update vite config

--- build/assets/stateDiagram-v2-4FDKWEC3-BvywSd3e.js
+++
@@ -1,1 +0,0 @@
-import{s as t,b as r,a,S as s}from"./chunk-DI55MBZ5-DHZnRiCO.js";import{_ as i}from"./index-BP2q6Uoi.js";import"./chunk-55IACEB6-Ciles1AI.js";import"./chunk-QN33PNHL-CjUTRRml.js";var l={parser:a,get db(){return new s(2)},renderer:r,styles:t,init:i(e=>{e.state||(e.state={}),e.state.arrowMarkerAbsolute=e.arrowMarkerAbsolute},"init")};export{l as diagram};
--- vite.config.js
+++ vite.config.js
@@ -24,7 +24,7 @@ const markdownPlugin = () => ({
 })
 
 export default defineConfig({
-  base: '/website/',
+  base: '/',
   plugins: [
     react(),
     markdownPlugin(),
